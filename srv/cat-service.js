5d3a63b374ffb6c623e223e3b9fa1c4d1fc21aba
Ben Krencker <benjamin.krencker@emmi.com>
2024-05-15 13:55:16 +0200

    Using BTP Objectstore service as AWS S3 API

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -3,7 +3,24 @@ const cds = require('@sap/cds');
 const { HDIContainers, Backups, Imports } = cds.entities('my');
 const { uuid } = cds.utils;
 
+
+
 const hana = require('@sap/hana-client');
+
+// 
+/**
+ * https://github.com/cap-js/attachments/blob/main/lib/aws-s3.js
+ * https://community.sap.com/t5/technology-blogs-by-members/working-with-files-in-cap-and-amazon-s3/ba-p/13427432
+ */
+const s3Credentials = JSON.parse(process.env.VCAP_SERVICES).objectstore[0].credentials;
+const { S3Client, GetObjectAttributesCommand, GetObjectCommand, HeadBucketCommand, HeadObjectCommand } = require("@aws-sdk/client-s3");
+const s3 = new S3Client({
+  region: s3Credentials.region,
+  credentials: {
+    accessKeyId: s3Credentials.access_key_id,
+    secretAccessKey: s3Credentials.secret_access_key,
+  }
+});
 
 class CatalogService extends cds.ApplicationService {
   init() {
@@ -11,9 +28,9 @@ class CatalogService extends cds.ApplicationService {
     async function _getHanaConnection() {
       const conn = hana.createConnection();
       const conn_params = {
-        serverNode: `${ cds.requires.hanadb.credentials.host }`,
-        uid: `${ cds.requires.hanadb.credentials.user }`,
-        pwd: `${ cds.requires.hanadb.credentials.pw }`
+        serverNode: `${cds.requires.hanadb.credentials.host}`,
+        uid: `${cds.requires.hanadb.credentials.user}`,
+        pwd: `${cds.requires.hanadb.credentials.pw}`
       };
 
       await conn.connect(conn_params);
@@ -21,13 +38,25 @@ class CatalogService extends cds.ApplicationService {
     };
 
     this.on('createBackup', async (req) => {
+      /**
+       * Check access to S3 Bucket
+       */
+      console.log('S3 Credentials', s3Credentials);
+      
+      const resp = await s3.send(new HeadBucketCommand({
+        "Bucket": s3Credentials.bucket
+      }));
+
+      console.log('S3 Bucket', resp);
+
+      
       console.log('Create Backup Action');
       console.log('req.data', JSON.stringify(req.data));
       console.log('req.params', JSON.stringify(req.params));
 
       const hdiContainer = await SELECT.one.from(req.subject, hdiContainer => {
         hdiContainer('*'),
-        hdiContainer.application('*')
+          hdiContainer.application('*')
       });
 
       console.log(`Create Backup for HDI Container`, JSON.stringify(hdiContainer));
@@ -51,19 +80,19 @@ class CatalogService extends cds.ApplicationService {
       console.log('Store Backup on S3 Path', awsS3FolderPath);
 
       await conn.exec('CREATE LOCAL TEMPORARY COLUMN TABLE #PARAMETERS LIKE _SYS_DI.TT_PARAMETERS;');
-      await conn.exec(`INSERT INTO #PARAMETERS (KEY, VALUE) VALUES ('target_path', '${ awsS3TargetPath }');`);
-      const exportResult = await conn.exec(`CALL _SYS_DI#BACKUP.EXPORT_CONTAINER_FOR_COPY('${ hdiContainer.containerId }', '', '', #PARAMETERS, ?, ?, ?);`);
+      await conn.exec(`INSERT INTO #PARAMETERS (KEY, VALUE) VALUES ('target_path', '${awsS3TargetPath}');`);
+      const exportResult = await conn.exec(`CALL _SYS_DI#BACKUP.EXPORT_CONTAINER_FOR_COPY('${hdiContainer.containerId}', '', '', #PARAMETERS, ?, ?, ?);`);
       await conn.exec(`DROP TABLE #PARAMETERS;`);
 
       console.log('Result:', JSON.stringify(exportResult));
 
-      let newBackupEntry = { 
-        ID: uuid(), 
-        created: createdTimestamp, 
-        hdiContainer_ID: hdiContainer.ID, 
+      let newBackupEntry = {
+        ID: uuid(),
+        created: createdTimestamp,
+        hdiContainer_ID: hdiContainer.ID,
         path: awsS3FolderPath,
-        IsActiveEntity: true, 
-        exportLogs: JSON.stringify(exportResult) 
+        IsActiveEntity: true,
+        exportLogs: JSON.stringify(exportResult)
       };
 
       console.log('Insert New Backup Entry', newBackupEntry);
@@ -82,7 +111,7 @@ class CatalogService extends cds.ApplicationService {
 
       const backup = await SELECT.one.from(req.subject, backup => {
         backup('*'),
-        backup.hdiContainer('*')
+          backup.hdiContainer('*')
       });
 
       console.log(`Restore Backup`, JSON.stringify(backup));
@@ -93,18 +122,18 @@ class CatalogService extends cds.ApplicationService {
       const awsS3SourcePath = `${awsS3.region}://${awsS3.access_key}:${awsS3.secret_key}@${awsS3.bucket_name}/${backup.path}`;
 
       await conn.exec('CREATE LOCAL TEMPORARY COLUMN TABLE #PARAMETERS LIKE _SYS_DI.TT_PARAMETERS;');
-      await conn.exec(`INSERT INTO #PARAMETERS (KEY, VALUE) VALUES ('source_path', '${ awsS3SourcePath }');`);
-      await conn.exec(`INSERT INTO #PARAMETERS (KEY, VALUE) VALUES ('original_container_name', '${ backup.hdiContainer.containerId }');`);
-      const importResult = await conn.exec(`CALL _SYS_DI#BACKUP.IMPORT_CONTAINER_FOR_COPY('${ backup.hdiContainer.containerId }', '', '', #PARAMETERS, ?, ?, ?);`);
+      await conn.exec(`INSERT INTO #PARAMETERS (KEY, VALUE) VALUES ('source_path', '${awsS3SourcePath}');`);
+      await conn.exec(`INSERT INTO #PARAMETERS (KEY, VALUE) VALUES ('original_container_name', '${backup.hdiContainer.containerId}');`);
+      const importResult = await conn.exec(`CALL _SYS_DI#BACKUP.IMPORT_CONTAINER_FOR_COPY('${backup.hdiContainer.containerId}', '', '', #PARAMETERS, ?, ?, ?);`);
       await conn.exec(`DROP TABLE #PARAMETERS;`);
 
       console.log('Result:', JSON.stringify(importResult));
 
-      let newImportEntry = { 
-        ID: uuid(), 
-        backup_ID: backup.ID, 
-        IsActiveEntity: true, 
-        importLogs: JSON.stringify(importResult) 
+      let newImportEntry = {
+        ID: uuid(),
+        backup_ID: backup.ID,
+        IsActiveEntity: true,
+        importLogs: JSON.stringify(importResult)
       };
 
       console.log('Insert New Backup Entry', newImportEntry);
